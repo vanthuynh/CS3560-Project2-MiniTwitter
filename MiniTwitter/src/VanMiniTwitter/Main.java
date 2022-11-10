e23d0fcecffd03c7fe43033dab4cb9caa268af6d
Van Huynh <vthuynh@cpp.edu>
2022-11-10 15:40:14 -0800

    first commit

---
+++ MiniTwitter/src/VanMiniTwitter/Main.java
@@ -0,0 +1,9 @@
+package VanMiniTwitter;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	}
+
+}
---
+++ MiniTwitter/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author htvan
+ *
+ */
+module MiniTwitter {
+}
